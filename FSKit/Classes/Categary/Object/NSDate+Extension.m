45b989497a1313e84795cdb1ad3bec95e6ed3913
fusen <fusen@163.com>
2016-03-14 11:36:51 +0800

    添加NSDate分类

---
+++ FSKit/Classes/Categary/Object/NSDate+Extension.m
@@ -0,0 +1,62 @@
+//
+//  NSDate+Extension.m
+//  test
+//
+//  Created by 四维图新 on 16/3/14.
+//  Copyright (c) 2016年 四维图新. All rights reserved.
+//
+
+#import "NSDate+Extension.h"
+
+static NSDateFormatter *dateFormatter;
+
+@implementation NSDate (Extension)
+
+
++(NSDateFormatter *)defaultFormatter
+{
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        dateFormatter = [[NSDateFormatter alloc]init];
+    });
+    return dateFormatter;
+}
+
++ (NSDate *)dateFromString:(NSString *)timeStr
+                    format:(NSString *)format
+{
+    NSDateFormatter *dateFormatter = [NSDate defaultFormatter];
+    [dateFormatter setDateFormat:format];
+    NSDate *date = [dateFormatter dateFromString:timeStr];
+    return date;
+}
+
++ (NSInteger)cTimestampFromDate:(NSDate *)date
+{
+    return (long)[date timeIntervalSince1970];
+}
+
+
++(NSInteger)cTimestampFromString:(NSString *)timeStr
+                          format:(NSString *)format
+{
+    NSDate *date = [NSDate dateFromString:timeStr format:format];
+    return [NSDate cTimestampFromDate:date];
+}
+
++ (NSString *)dateStrFromCstampTime:(NSInteger)timeStamp
+                     withDateFormat:(NSString *)format
+{
+    NSDate *date = [NSDate dateWithTimeIntervalSince1970:timeStamp];
+    return [NSDate datestrFromDate:date withDateFormat:format];
+}
+
++ (NSString *)datestrFromDate:(NSDate *)date
+               withDateFormat:(NSString *)format
+{
+    NSDateFormatter* dateFormat = [NSDate defaultFormatter];
+    [dateFormat setDateFormat:format];
+    return [dateFormat stringFromDate:date];
+}
+
+@end
